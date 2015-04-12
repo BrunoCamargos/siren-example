9e018329c92b1cb78e9d4eec34fa4cd2cb78ad56
Bruno Camargos <bruno.camargos@gmail.com>
2015-04-11 22:28:37 -0300

    Adicionado o delete

--- routes/categories.js
+++ routes/categories.js
@@ -7,50 +7,50 @@ require('../models/category');
 var categories = db.model('Category');
 
 router.get('/', function(req, res, next) {
-	categories.find({}, function(err, data) {
+	categories.find({}, function(err, categories) {
 		if (err) {
 			next(err);
 		} else {
-
-			// console.log(data[0]._id);
-			var result = new Achelous('categories');
-
-			var i = 0;
-			for (i; i < data.length; i++) {
-				result.addEntity({
-					class: ['category'],
-					properties: {
-						id: data[i]._id,
-						name: data[i].name
-					},
-					rel: 'item',
-					title: data[i].name,
-					links: [{
-						rel: 'self',
-						title: data[i].name,
-						href: 'http://siren-example.herokuapp.com/categories/' + data[i]._id
-					}]
-				});
-			}
-
-			result.addAction({
-				name: 'add-category',
-				method: 'POST',
-				href: 'http://siren-example.herokuapp.com/categories/',
-				title: 'Add Category',
-				type: 'application/json',
-				fields: [{
-					name: 'name',
-					type: 'text',
-					title: 'Category name'
-				}]
-			});
-
-			result.addLink('self', 'http://siren-example.herokuapp.com/categories/');
-			res.status(200).send(result);
+			res.status(200).send(createCategoriesResult(categories));
 		}
 	});
 });
+
+function createCategoriesResult(categories) {
+	var result = new Achelous('categories');
+	var i = 0;
+	for (i; i < categories.length; i++) {
+		result.addEntity({
+			class: ['category'],
+			properties: {
+				id: categories[i]._id,
+				name: categories[i].name
+			},
+			rel: 'item',
+			title: categories[i].name,
+			links: [{
+				rel: 'self',
+				title: categories[i].name,
+				href: 'http://siren-example.herokuapp.com/categories/' + categories[i]._id
+			}]
+		});
+	}
+
+	result.addAction({
+		name: 'add-category',
+		method: 'POST',
+		href: 'http://siren-example.herokuapp.com/categories/',
+		title: 'Add Category',
+		type: 'application/json',
+		fields: [{
+			name: 'name',
+			type: 'text',
+			title: 'Category name'
+		}]
+	});
+
+	result.addLink('self', 'http://siren-example.herokuapp.com/categories/');
+}
 
 router.get('/:id', function(req, res) {
 	categories.findById(req.params.id).exec(function(err, category) {
@@ -86,8 +86,22 @@ function createCategoryResult(category) {
 
 	result.addLink('self', 'http://siren-example.herokuapp.com/categories/' + category._id);
 	result.addLink('categories', 'http://siren-example.herokuapp.com/categories/');
-	return result;
 
+	result.addAction({
+		name: 'del-category',
+		method: 'DELETE',
+		href: 'http://siren-example.herokuapp.com/categories/',
+		title: 'Delete Category',
+		// type: 'application/json',
+		fields: [{
+			name: 'id',
+			type: 'text',
+			title: 'Category ID',
+			value: category._id
+		}]
+	});
+
+	return result;
 }
 
 router.post('/', function(req, res, next) {
@@ -99,7 +113,23 @@ router.post('/', function(req, res, next) {
 		if (err) {
 			next(err);
 		} else {
-			res.status(200).send(createCategoryResult(category));
+			res.status(201).send(createCategoryResult(category));
+		}
+	});
+});
+
+router.delete('/:id', function(req, res, next) {
+	console.log(req.params.id);
+	console.log(req.body);
+
+	categories.remove({
+		'_id': req.params.id
+	}, function(err, data) {
+		if (err) {
+			next(err);
+		} else {
+			console.log(data);
+			res.status(200).send(createCategoryResult(data));
 		}
 	});
 });
